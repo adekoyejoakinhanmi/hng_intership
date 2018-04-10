a8cd3ed18cf835e8c4abbae4d7fdcfa2230ab470
Adekoyejo Akinhanmi <koyesavedbygrace@yahoo.com>
2018-04-10 09:25:23 +0100

    completed stage one tasks

---
+++ stage_1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HNG Intership Stage 1</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet"> 
+    <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <h1>
+            <?php
+            echo date("h:i");
+            ?>
+        </h1>
+    </header>
+</body>
+</html>
